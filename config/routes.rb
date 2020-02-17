2dcd05b2a599f29de1df45b8e1ce7857a617ead1
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-17 15:24:04 -0500

    railes files updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/new', to: 'games#new', as: :new
+  post '/score', to: 'score#create', as: :score
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
