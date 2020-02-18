719afbfc9af54040dc780b6af84bb4524591aea6
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-18 10:33:45 -0500

    Added some styles to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/new', to: 'games#new', as: :new
-  post '/score', to: 'score#create', as: :score
+  post '/score', to: 'score#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
